71da17981baddba55905af478f656e463a3cf082
TgB <1214419004@s.tusd1.org>
2026-05-08 18:47:56 +0000

    m, bn

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
 
 let clicks = 0;
-let clickValue = 1
+let clickValue = 1;
+
+let savedTears = localStorage.getItem("savedTears");
+if (savedTears !== null) {
+  clicks = Number(savedTears);
+}
+
+
 
 let tearDisplay = document.getElementById("tear-display");
 
 tearDisplay.innerText = "Tears: " + clicks;
 
 function handleClick() {
-    console.log("Click");
-
     clicks = clicks + clickValue;
 
     tearDisplay.innerText = "Tears: " + clicks;
 
+    localStorage.setItem("savedTears", clicks);
 }
 
+function buyUpgrade(event) {
+  let upgradeCost = 10;
+  
+  if (clicks >= upgradeCost) {
+    clicks - upgradeCost;
+    clickValue = clickValue++;
+    tearDisplay.innerText = "Tears: " + clicks;
+    localStorage.setItem("savedTears", clicks);
+    .addEventListener(“click”, myFunction); 
+  } 
+}
