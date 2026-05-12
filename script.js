5092690ad61e1dc63b5292577d596779ef76385e
TgB <1214419004@s.tusd1.org>
2026-05-12 18:36:54 +0000

    asda

--- script.js
+++ script.js
@@ -13,30 +13,59 @@ if (savedClickValue !== null) {
 }
 
 let tearDisplay = document.getElementById("tear-display");
-
-tearDisplay.innerText = "Tears: " + clicks;
+tearDisplay.innerText = "Tears: " + formatNumber(clicks);
 
 function handleClick() {
     clicks = clicks + clickValue;
 
-    tearDisplay.innerText = "Tears: " + clicks;
+    tearDisplay.innerText = "Tears: " + formatNumber(clicks);
 
     localStorage.setItem("savedTears", clicks);
 }
 
-
-let upgradeBtn = document.getElementById("upgrade-btn");
 
 function buyUpgrade() {
   let upgradeCost = 10;
   
   if (clicks >= upgradeCost) {
-    console.log("f")
     clicks = clicks - upgradeCost;
     clickValue = clickValue + 1;
     localStorage.setItem("savedClickValue", clickValue);
-    tearDisplay.innerText = "Tears: " + clicks;
+    tearDisplay.innerText = "Tears: " + formatNumber(clicks);
     localStorage.setItem("savedTears", clicks);
   } 
 }
 
+function buyUpgrade2() {
+  let upgradeCost = 200;
+  
+  if (clicks >= upgradeCost) {
+    clicks = clicks - upgradeCost;
+    clickValue = clickValue + 10;
+    localStorage.setItem("savedClickValue", clickValue);
+    tearDisplay.innerText = "Tears: " + formatNumber(clicks);
+    localStorage.setItem("savedTears", clicks);
+  } 
+}
+
+function buyUpgrade3() {
+  let upgradeCost = 1500;
+  
+  if (clicks >= upgradeCost) {
+    clicks = clicks - upgradeCost;
+    clickValue = clickValue + 75;
+    localStorage.setItem("savedClickValue", clickValue);
+    tearDisplay.innerText = "Tears: " + formatNumber(clicks);
+    localStorage.setItem("savedTears", clicks);
+  } 
+}
+
+function formatNumber(num) {
+  if (num >= 1000000) {
+    return (num / 1000000).toFixed(2) + " M";
+  } else if (num >= 1000) {
+    return (num / 1000).toFixed(2) + " k";
+  } else {
+    return num;
+  }
+}
